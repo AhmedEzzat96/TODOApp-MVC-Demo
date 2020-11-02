676a8e6449188708b7f288768a2b344ccfc9fd79
Ahmed Ezzat <ahmed.ezzat2410@gmail.com>
2020-11-02 06:42:16 +0200

    Cell Border&Shadow

--- TODOApp-MVC-Demo/View/TaskCell/TaskCell.swift
+++ TODOApp-MVC-Demo/View/TaskCell/TaskCell.swift
@@ -14,7 +14,7 @@ class TaskCell: UITableViewCell {
     // MARK:- Lifecycle Methods
     override func awakeFromNib() {
         super.awakeFromNib()
-        shadowAndBorderForCell(yourTableViewCell: self)
+        shadowAndBorderForCell()
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
@@ -26,20 +26,4 @@ class TaskCell: UITableViewCell {
         descriptionLabel.text = task.description
     }
     
-}
-
-extension UITableViewCell{
-    func shadowAndBorderForCell(yourTableViewCell : UITableViewCell){
-        // SHADOW AND BORDER FOR CELL
-        yourTableViewCell.contentView.layer.cornerRadius = 20
-        yourTableViewCell.contentView.layer.borderWidth = 1
-        yourTableViewCell.contentView.layer.borderColor = UIColor.darkGray.cgColor
-        yourTableViewCell.contentView.layer.masksToBounds = true
-        yourTableViewCell.layer.shadowColor = UIColor.gray.cgColor
-        yourTableViewCell.layer.shadowOffset = CGSize(width: 0, height: 2)
-        yourTableViewCell.layer.shadowRadius = 2.0
-        yourTableViewCell.layer.shadowOpacity = 1.0
-        yourTableViewCell.layer.masksToBounds = false
-        yourTableViewCell.layer.shadowPath = UIBezierPath(roundedRect:yourTableViewCell.bounds, cornerRadius:yourTableViewCell.contentView.layer.cornerRadius).cgPath
-    }
 }
